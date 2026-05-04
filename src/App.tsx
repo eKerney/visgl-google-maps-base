7d800e4675adb1b9b186dc62870446f17a6341fa
ekerney <eric.m.kerney@wmich.edu>
2026-05-03 23:57:53 -0400

    add deckgl react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import { APIProvider, Circle, limitTiltRange, Map, Marker } from '@vis.gl/react-google-maps';
 import './App.css';
+import DeckGL from '@deck.gl/react';
 
 const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY || 'YOUR_PLACEHOLDER_KEY';
 
 function App() {
-  const defaultCenter = { lat: 37.7749, lng: -122.4194 }; // San Francisco
+  const defaultCenter = { lat: 41.865, lng: -87.64 };
+  const INITIAL_VIEW_STATE = {
+    latitude: 41.865,
+    longitude: -87.64,
+    zoom: 10,
+  };
 
   return (
     <div className="map-container">
+
       <APIProvider apiKey={API_KEY}>
-        <Map
-          style={{ width: '100vw', height: '100vh' }}
-          defaultCenter={defaultCenter}
-          defaultZoom={13}
-          gestureHandling={'greedy'}
-          disableDefaultUI={false}
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={true}
         >
-          <Marker position={defaultCenter} />
-        </Map>
+          <Map
+            style={{ width: '100vw', height: '100vh' }}
+            defaultCenter={defaultCenter}
+            defaultZoom={13}
+            gestureHandling={'greedy'}
+            disableDefaultUI={false}
+          >
+            <Marker position={defaultCenter} />
+            <Circle
+              center={{ lat: 42, lng: -87.8 }}
+              radius={10000}
+              fillColor={'#0088ff'}
+              fillOpacity={0.3}
+              strokeColor={'#0088ff'}
+              strokeWeight={2}
+            />
+          </Map>
+        </DeckGL>
       </APIProvider>
-    </div>
+    </div >
   );
 }
 
 export default App;
+// onViewStateChange={limitTiltRange({ INITIAL_VIEW_STATE })}>
