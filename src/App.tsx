8911d2ca49db3f2f08055b0fa953897203a2dd4b
ekerney <eric.m.kerney@wmich.edu>
2026-05-11 14:07:15 -0400

    testing google react maps components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { AdvancedMarker, APIProvider, Circle, Map } from '@vis.gl/react-google-maps';
+import { AdvancedMarker, APIProvider, Circle, ControlPosition, InfoWindow, Map, MapControl, useAdvancedMarkerRef } from '@vis.gl/react-google-maps';
 import './App.css';
 import { DeckGlOverlay } from './DeckGLOverlay';
 import { layers } from './layers';
-// import { PlaceOverview } from '@googlemaps/extended-component-library/place_overview.js';
-import { PlaceDirectionsButton, PlaceOverview } from '@googlemaps/extended-component-library/react';
+import { useCallback, useState } from 'react';
 
 const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
@@ -11,20 +10,34 @@ function App() {
   // const defaultCenter = { lat: 41.865, lng: -87.64 }; //chicago
   const defaultCenter = { lat: 37.7, lng: -122.4 };
 
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const [infoWindowShown, setInfoWindowShown] = useState(false);
+  const handleMarkerClick = useCallback(() => setInfoWindowShown(isShown => !isShown), []);
+  const handleClose = useCallback(() => setInfoWindowShown(false), []);
+
   return (
     <div className="map-container">
 
-      <APIProvider apiKey={API_KEY}>
+      <APIProvider apiKey={API_KEY} version='beta'>
         <Map
           style={{ width: '100vw', height: '100vh' }}
           defaultCenter={defaultCenter}
-          defaultZoom={10}
+          defaultZoom={4}
           gestureHandling={'greedy'}
           disableDefaultUI={false}
           mapId={'4f6dde3310be51d7'}
         >
           <DeckGlOverlay layers={[layers()]} />
-          <AdvancedMarker position={defaultCenter} />
+          <MapControl position={ControlPosition.LEFT_CENTER}>
+            .. any component here will be added to the control-containers of the
+            google map instance ..
+          </MapControl>
+          {infoWindowShown &&
+            <InfoWindow anchor={marker} style={{ color: "black" }} onClose={handleClose}>
+              <p>The content of the info window is here</p>
+            </InfoWindow>
+          }
+          <AdvancedMarker position={defaultCenter} ref={markerRef} onClick={handleMarkerClick} />
           <Circle
             center={{ lat: 42, lng: -87.8 }}
             radius={10000}
@@ -33,11 +46,6 @@ function App() {
             strokeColor={'#0088ff'}
             strokeWeight={2}
           />
-          {/* <PlaceOverview size="large" place="ChIJ39Y-tdg1fYcRQcZcBb499do" googleLogoAlreadyDisplayed> */}
-          {/*   <PlaceDirectionsButton slot="action" origin={origin} ariaLabel="see directions on Google Maps"> */}
-          {/*     Directions */}
-          {/*   </PlaceDirectionsButton> */}
-          {/* </PlaceOverview> */}
         </Map>
       </APIProvider>
     </div >
